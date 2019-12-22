5808d3026f3518a3b610ce8001d004a4a42f21d4
cloneable <1573652017@qq.com>
2019-12-22 22:52:22 +0800

    realize checking winner

--- tdd-java-ch03-tic-tac-toe/src/main/java/tictactoe/TicTacToe.java
+++ tdd-java-ch03-tic-tac-toe/src/main/java/tictactoe/TicTacToe.java
@@ -1,11 +1,15 @@
 package tictactoe;
 
+import java.util.SimpleTimeZone;
+
 public class TicTacToe {
+    private final int SIZE;
     private Character[][] board;
     private char latestPlayer;
 
     public TicTacToe() {
         board = new Character[][]{{'\0', '\0', '\0'}, {'\0', '\0', '\0'}, {'\0', '\0', '\0'}};
+        SIZE = 3;
     }
 
     private void storePoint(int x, int y) {
@@ -17,10 +21,52 @@ public class TicTacToe {
         board[x - 1][y - 1] = latestPlayer;
     }
 
-    public void play(Point point) {
+    public String play(Point point) {
         point.checkXAxis();
         point.checkYAxis();
         storePoint(point.getX(), point.getY());
+
+        if (isWin()) {
+            return latestPlayer + " is winner";
+        } else if (isDraw()) {
+            return "The result is draw";
+        }
+
+        return "No winner";
+    }
+
+    private boolean isDraw() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (board[i][j] == '\0') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean isWin() {
+        for (int i = 0; i < SIZE; i++) {
+            if (board[0][i] + board[1][i] +board[2][i] == (latestPlayer * SIZE)) {
+                return true;
+            }
+
+            if (board[i][0] + board[i][1] +board[i][2] == (latestPlayer * SIZE)) {
+                return true;
+            }
+
+        }
+
+        if (board[0][0] + board[1][1] + board[2][2] == (latestPlayer * SIZE)) {
+            return true;
+        }
+
+        if (board[2][0] + board[1][1] + board[0][2] == (latestPlayer * SIZE)) {
+            return true;
+        }
+
+        return false;
     }
 
     public char nextPlayer() {
--- tdd-java-ch03-tic-tac-toe/src/test/java/tictactoe/TicTacToeTest.java
+++ tdd-java-ch03-tic-tac-toe/src/test/java/tictactoe/TicTacToeTest.java
@@ -53,4 +53,65 @@ public class TicTacToeTest {
         ticTacToe.play(new Point(1, 1));
         assertEquals('O', ticTacToe.nextPlayer());
     }
+
+    @Test
+    public void should_no_winner_when_play() {
+        String result = ticTacToe.play(new Point(1, 1));
+        assertEquals("No winner", result);
+    }
+
+    @Test
+    public void should_winner_X_when_play_given_X_whole_horizon_line() {
+        ticTacToe.play(new Point(1, 1)); // X
+        ticTacToe.play(new Point(1, 2)); // O
+        ticTacToe.play(new Point(2, 1)); // X
+        ticTacToe.play(new Point(2, 2)); // O
+        String result = ticTacToe.play(new Point(3, 1));// X
+        assertEquals("X is winner", result);
+    }
+
+    @Test
+    public void should_winner_O_when_play_given_O_whole_vertical_line() {
+        ticTacToe.play(new Point(2, 1)); // X
+        ticTacToe.play(new Point(1, 1)); // O
+        ticTacToe.play(new Point(3, 1)); // X
+        ticTacToe.play(new Point(1, 2)); // O
+        ticTacToe.play(new Point(2, 2)); // X
+        String result = ticTacToe.play(new Point(1, 3));// O
+        assertEquals("O is winner", result);
+    }
+
+    @Test
+    public void should_winner_X_when_play_given_X_whole_top_bottom_diagonal_line() {
+        ticTacToe.play(new Point(1, 1)); // X
+        ticTacToe.play(new Point(1, 2)); // O
+        ticTacToe.play(new Point(2, 2)); // X
+        ticTacToe.play(new Point(1, 3)); // O
+        String result = ticTacToe.play(new Point(3, 3));// X
+        assertEquals("X is winner", result);
+    }
+
+    @Test
+    public void should_winner_X_when_play_given_X_whole_bottom_top_diagonal_line() {
+        ticTacToe.play(new Point(1, 3)); // X
+        ticTacToe.play(new Point(1, 1)); // O
+        ticTacToe.play(new Point(2, 2)); // X
+        ticTacToe.play(new Point(1, 2)); // O
+        String result = ticTacToe.play(new Point(3, 1));// X
+        assertEquals("X is winner", result);
+    }
+
+    @Test
+    public void should_draw_when_play_given_filled_board_and_no_winner() {
+        ticTacToe.play(new Point(1, 1));
+        ticTacToe.play(new Point(1, 2));
+        ticTacToe.play(new Point(1, 3));
+        ticTacToe.play(new Point(2, 1));
+        ticTacToe.play(new Point(2, 3));
+        ticTacToe.play(new Point(2, 2));
+        ticTacToe.play(new Point(3, 1));
+        ticTacToe.play(new Point(3, 3));
+        String result = ticTacToe.play(new Point(3, 2));
+        assertEquals("The result is draw", result);
+    }
 }
