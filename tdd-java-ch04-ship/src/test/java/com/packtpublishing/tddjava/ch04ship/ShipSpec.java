f9d5ebbe7bffc8184d34e9b437325f7fcdab8bb6
Cloneable <cloneable@163.com>
2019-12-26 08:59:40 +0800

    initialize ship instance

--- tdd-java-ch04-ship/src/test/java/com/packtpublishing/tddjava/ch04ship/ShipSpec.java
+++ tdd-java-ch04-ship/src/test/java/com/packtpublishing/tddjava/ch04ship/ShipSpec.java
@@ -7,4 +7,9 @@ import static org.testng.Assert.*;
 @Test
 public class ShipSpec {
 
+    public void should_start_point_and_direction_when_initialize_ship_given_point_and_direction() {
+        Location initLocation = new Location(new Point(0, 0), Direction.EAST);
+        Ship ship = new Ship(initLocation);
+        assertEquals(ship.getLocation(), initLocation);
+    }
 }
